1353060bb1cb63335672ab742e61e6b0dd3e1e2e
ln905 <80632340+ln905@users.noreply.github.com>
2021-04-17 21:48:55 +0700

    Create Buoi6.js

---
+++ Buoi6/Buoi6.js
@@ -0,0 +1,28 @@
+var Class = 
+[
+  {Name: "NAL", Age: 16},
+  {Name: "Khoa", Age: 13},
+  {Name: "Sơn", Age: 13},
+  {Name: "Lan", Age: 14},
+  {Name: "Nam", Age: 16},
+  {Name: "Trung", Age: 16},
+  {Name: "Khôi", Age: 16},
+  {Name: "Huy", Age: 17},
+  {Name: "Hoàng", Age: 17},
+  {Name: "Đăng", Age: 17},
+  {Name: "Vinh", Age: 17},
+  {Name: "Giang", Age: 22},
+]
+
+console.log(Class)
+let total_Age = 0;
+for (i=0; i<Class.length; i++)
+{total_Age = total_Age + Class[i].Age;}
+console.log(total_Age)
+
+  
+for (i=999; i>=0; i-=3)
+{
+  console.log(i)
+}
+
