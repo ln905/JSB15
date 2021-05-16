f9cf4bf25ab9e67b662d2a6bdf1662f4fee5200c
ln905 <80632340+ln905@users.noreply.github.com>
2021-05-16 12:35:08 +0700

    Update ChangeBGColorRGB.js

--- Buoi 10/ChangeBGColorRGB.js
+++ Buoi 10/ChangeBGColorRGB.js
@@ -1,13 +1,12 @@
 document.getElementById('clickme').addEventListener('click', all)
+document.getElementById('stop').addEventListener('click', clear)
+let a
 function all () {
-    var a = setInterval(changeBG,500)
-    document.getElementById('stop').addEventListener('click', clear)
-
-    function changeBG () {
-        document.body.style.backgroundColor= 'rgb('+Math.floor(Math.random() * 256)+','+Math.floor(Math.random() * 256)+','+Math.floor(Math.random() * 256)+')'
-                         }
-
-    function clear (){
-        clearInterval(a)
-                     }
+    a = setInterval(changeBG,500)
 }
+function changeBG () {
+    document.body.style.backgroundColor= 'rgb('+Math.floor(Math.random() * 256)+','+Math.floor(Math.random() * 256)+','+Math.floor(Math.random() * 256)+')'
+    }
+function clear (){
+        clearInterval(a)
+    }
